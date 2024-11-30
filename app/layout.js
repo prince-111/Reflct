1e31a2374d880f0e5fe7d6231d8275034bed9615
Prince Kumar <princesd991@gmail.com>
2024-11-30 16:26:30 +0530

    add header footer 🛹

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        <div className="bg-[url('/bg.jpg')] opacity-50 fixed -z-10 inset-0" />
+        <Header />
+        <main className="min-h-screen">{children}</main>
+
+        <footer className="bg-orange-300 py-6 bg-opacity-10">
+          <div className="container mx-auto px-4 text-center text-gray-900">
+            <p>Made with 💗 by Prince</p>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
---
+++ components/header.jsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="Reflct Logo"
+            width={200}
+            height={60}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+
+        <div className="flex items-center gap-4"></div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
